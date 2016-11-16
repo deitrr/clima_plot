4ad74affa2bf1bb1ffb3c5c05172c839daac0d63
Russell Deitrick <deitrr@u.washington.edu>
2016-11-16 14:41:35 -0800

    working on plotting multiple directories in the same plot

--- climaplot.py
+++ climaplot.py
@@ -153,181 +153,188 @@ def clim_evol(plname,dir='.',xrange=False,orbit=False):
   PDF format plot with name 'evol_<dir>.pdf'
   
   """
-
-  out = vplot.GetOutput(dir)
+  if not isinstance(dir,(list,tuple)):
+    dir = [dir]
   
-  ctmp = 0
-  for p in range(len(out.bodies)):
-    if out.bodies[p].name == plname:
-      body = out.bodies[p]
-      ctmp = 1
+  nfiles = len(dir)
+
+  for ii in np.arange(nfiles):
+    pdb.set_trace()
+    out = vplot.GetOutput(dir[ii])
+  
+    ctmp = 0
+    for p in range(len(out.bodies)):
+      if out.bodies[p].name == plname:
+        body = out.bodies[p]
+        ctmp = 1
+      else:
+        if p == len(out.bodies)-1 and ctmp == 0:
+          raise Exception("Planet %s not found in folder %s"%(plname,folders[j]))
+  
+    try:
+      ecc = body.Eccentricity
+    except:
+      ecc = np.zeros_like(body.Time)+getattr(out.log.initial,plname).Eccentricity
+    
+    try:
+      inc = body.Inc
+    except:
+      inc = np.zeros_like(body.Time)
+    
+    try:
+      obl = body.Obliquity
+    except:
+      obltmp = getattr(out.log.initial,plname).Obliquity
+      if obltmp.unit == 'rad':
+        obltmp *= 180/np.pi
+      obl = np.zeros_like(body.Time)+obltmp
+
+    f = open(dir[ii]+'/'+plname+'.in','r')
+    lines = f.readlines()
+    f.close()
+    for i in range(len(lines)):
+      if lines[i].split() != []:
+        if lines[i].split()[0] == 'dRotPeriod':
+          P = -1*np.float(lines[i].split()[1])  
+
+    try:
+      longp = (body.ArgP + body.LongA + body.PrecA)*np.pi/180.0
+    except:
+      longp = body.PrecA*np.pi/180.0
+    
+    esinv = ecc*np.sin(longp)*np.sin(obl*np.pi/180.)
+
+    if orbit == True:
+      fig = plt.figure(figsize=(16,12))
     else:
-      if p == len(out.bodies)-1 and ctmp == 0:
-        raise Exception("Planet %s not found in folder %s"%(plname,folders[j]))
-  
-  try:
-    ecc = body.Eccentricity
-  except:
-    ecc = np.zeros_like(body.Time)+getattr(out.log.initial,plname).Eccentricity
+      fig = plt.figure(figsize=(10,14))
+    fig.suptitle('$e_0 = %f, i_0 = %f^{\circ}, \psi_0 = %f^{\circ}, P_{rot} = %f$ d'%(ecc[0],inc[0],obl[0],P),fontsize=20) 
+    fig.subplots_adjust(wspace=0.3)
+
+    lats = np.unique(body.Latitude)
+    nlats = len(lats)
+    ntimes = len(body.Time)
     
-  try:
-    inc = body.Inc
-  except:
-    inc = np.zeros_like(body.Time)
-    
-  try:
-    obl = body.Obliquity
-  except:
-    obltmp = getattr(out.log.initial,plname).Obliquity
-    if obltmp.unit == 'rad':
-      obltmp *= 180/np.pi
-    obl = np.zeros_like(body.Time)+obltmp
-
-  f = open(dir+'/'+plname+'.in','r')
-  lines = f.readlines()
-  f.close()
-  for i in range(len(lines)):
-    if lines[i].split() != []:
-      if lines[i].split()[0] == 'dRotPeriod':
-        P = -1*np.float(lines[i].split()[1])  
-
-  try:
-    longp = (body.ArgP + body.LongA + body.PrecA)*np.pi/180.0
-  except:
-    longp = body.PrecA*np.pi/180.0
-    
-  esinv = ecc*np.sin(longp)*np.sin(obl*np.pi/180.)
-
-  if orbit == True:
-    fig = plt.figure(figsize=(16,12))
-  else:
-    fig = plt.figure(figsize=(10,14))
-  fig.suptitle('$e_0 = %f, i_0 = %f^{\circ}, \psi_0 = %f^{\circ}, P_{rot} = %f$ d'%(ecc[0],inc[0],obl[0],P),fontsize=20) 
-  fig.subplots_adjust(wspace=0.3)
-
-  lats = np.unique(body.Latitude)
-  nlats = len(lats)
-  ntimes = len(body.Time)
-
-  # plot temperature
-  temp = np.reshape(body.TempLat,(ntimes,nlats))
-  if orbit == True:
-    ax1 = plt.subplot(4,2,1)
-  else:
-    ax1 = plt.subplot(5,1,1)
-  pos = ax1.figbox.get_points()
-  c = plt.contourf(body.Time,lats,temp.T,cmap='plasma')
-  plt.ylabel('Latitude')
-  plt.title(r'Surface Temp [$^{\circ}$C]')
-  plt.ylim(-90,90)
-  plt.yticks([-60,-30,0,30,60])
-  if xrange:
-    plt.xlim(xrange)
-  plt.colorbar(c,cax=plt.axes([pos[1,0]+0.01,pos[0,1],0.01,pos[1,1]-pos[0,1]]))
-  
-  # plot albedo
-  alb = np.reshape(body.AlbedoLat,(ntimes,nlats))
-  if orbit == True:
-    ax2 = plt.subplot(4,2,3)
-  else:
-    ax2 = plt.subplot(5,1,3)
-  pos = ax2.figbox.get_points()
-  c = plt.contourf(body.Time,lats,alb.T,cmap='Blues_r')
-  plt.ylabel('Latitude')
-  plt.title('Albedo (TOA)')
-  plt.ylim(-90,90)
-  plt.yticks([-60,-30,0,30,60])
-  if xrange:
-    plt.xlim(xrange)
-  plt.colorbar(c,cax=plt.axes([pos[1,0]+0.01,pos[0,1],0.01,pos[1,1]-pos[0,1]]))
-  
-
-  # plot ice height
-  ice = np.reshape(body.IceHeight,(ntimes,nlats))
-  if orbit == True:
-    ax3 = plt.subplot(4,2,5)
-  else:
-    ax3 = plt.subplot(5,1,4)
-  pos = ax3.figbox.get_points()
-  c = plt.contourf(body.Time,lats,ice.T,cmap='Blues_r')
-  plt.ylabel('Latitude')
-  plt.title('Ice sheet height [m]')
-  plt.ylim(-90,90)
-#   plt.xlim(0,2e6)
-  plt.yticks([-60,-30,0,30,60])
-  if xrange:
-    plt.xlim(xrange)
-  plt.colorbar(c,cax=plt.axes([pos[1,0]+0.01,pos[0,1],0.01,pos[1,1]-pos[0,1]]))
-  # ax3p = ax3.twinx()
-#   plt.plot(body.Time,esinv,linestyle = 'solid',marker='None',color='salmon',linewidth=2)
-  
-
-  # plot bedrock
-  brock = np.reshape(body.BedrockH,(ntimes,nlats))
-  if orbit == True:
-    ax4 = plt.subplot(4,2,7)
-  else:
-    ax4 = plt.subplot(5,1,5)
-  pos = ax4.figbox.get_points()
-  c = plt.contourf(body.Time,lats,brock.T,cmap='Reds_r')
-  plt.ylabel('Latitude')
-  plt.title('Bedrock height [m]')
-  plt.ylim(-90,90)
-  plt.yticks([-60,-30,0,30,60])
-  plt.xlabel('Time [years]')
-  if xrange:
-    plt.xlim(xrange)
-  plt.colorbar(c,cax=plt.axes([pos[1,0]+0.01,pos[0,1],0.01,pos[1,1]-pos[0,1]]))
-  
-
-  # plot insolation
-  insol = np.reshape(body.AnnInsol,(ntimes,nlats))
-  if orbit == True:
-    ax5 = plt.subplot(4,2,2)
-  else:
-    ax5 = plt.subplot(5,1,2)
-  pos = ax5.figbox.get_points()
-  c = plt.contourf(body.Time,lats,insol.T,cmap='plasma')
-  plt.ylabel('Latitude')
-  plt.title(r'Annual average insolation [w/m$^2$]')
-  plt.ylim(-90,90)
-  plt.yticks([-60,-30,0,30,60])
-  if xrange:
-    plt.xlim(xrange)
-  plt.colorbar(c,cax=plt.axes([pos[1,0]+0.01,pos[0,1],0.01,pos[1,1]-pos[0,1]]))
-
-  if orbit == True:
-    #obliquity
-    plt.subplot(4,2,4)
-    plt.plot(body.Time,obl,linestyle = 'solid',marker='None',color='darkblue',linewidth =2)
-    plt.ylabel('Obliquity')
+    pdb.set_trace()
+    # plot temperature
+    temp = np.reshape(body.TempLat,(ntimes,nlats))
+    if orbit == True:
+      ax1 = plt.subplot(4,2,1)
+    else:
+      ax1 = plt.subplot(5,nfiles,ii+1)
+    pos = ax1.figbox.get_points()
+    c = plt.contourf(body.Time,lats,temp.T,cmap='plasma')
+    plt.ylabel('Latitude')
+    plt.title(r'Surface Temp [$^{\circ}$C]')
+    plt.ylim(-90,90)
+    plt.yticks([-60,-30,0,30,60])
     if xrange:
       plt.xlim(xrange)
-
-    #eccentricity
-    plt.subplot(4,2,6)
-    plt.plot(body.Time,ecc,linestyle = 'solid',marker='None',color='darkorchid',linewidth =2)
-    plt.ylabel('Eccentricity')
+    plt.colorbar(c,cax=plt.axes([pos[1,0]+0.01,pos[0,1],0.01,pos[1,1]-pos[0,1]]))
+  
+    # plot albedo
+    alb = np.reshape(body.AlbedoLat,(ntimes,nlats))
+    if orbit == True:
+      ax2 = plt.subplot(4,2,3)
+    else:
+      ax2 = plt.subplot(5,nfiles,ii+2*nfiles+1)
+    pos = ax2.figbox.get_points()
+    c = plt.contourf(body.Time,lats,alb.T,cmap='Blues_r')
+    plt.ylabel('Latitude')
+    plt.title('Albedo (TOA)')
+    plt.ylim(-90,90)
+    plt.yticks([-60,-30,0,30,60])
     if xrange:
       plt.xlim(xrange)
+    plt.colorbar(c,cax=plt.axes([pos[1,0]+0.01,pos[0,1],0.01,pos[1,1]-pos[0,1]]))
+  
 
-    #e sin(obl) sin varpi
-    plt.subplot(4,2,8)
-    plt.plot(body.Time,esinv,linestyle = 'solid',marker='None',color='salmon',linewidth=2)
-    plt.ylabel('COPP')
+    # plot ice height
+    ice = np.reshape(body.IceHeight,(ntimes,nlats))
+    if orbit == True:
+      ax3 = plt.subplot(4,2,5)
+    else:
+      ax3 = plt.subplot(5,nfiles,ii+3*nfiles+1)
+    pos = ax3.figbox.get_points()
+    c = plt.contourf(body.Time,lats,ice.T,cmap='Blues_r')
+    plt.ylabel('Latitude')
+    plt.title('Ice sheet height [m]')
+    plt.ylim(-90,90)
+  #   plt.xlim(0,2e6)
+    plt.yticks([-60,-30,0,30,60])
+    if xrange:
+      plt.xlim(xrange)
+    plt.colorbar(c,cax=plt.axes([pos[1,0]+0.01,pos[0,1],0.01,pos[1,1]-pos[0,1]]))
+    # ax3p = ax3.twinx()
+  #   plt.plot(body.Time,esinv,linestyle = 'solid',marker='None',color='salmon',linewidth=2)
+  
+
+    # plot bedrock
+    brock = np.reshape(body.BedrockH,(ntimes,nlats))
+    if orbit == True:
+      ax4 = plt.subplot(4,2,7)
+    else:
+      ax4 = plt.subplot(5,nfiles,ii+4*nfiles+1)
+    pos = ax4.figbox.get_points()
+    c = plt.contourf(body.Time,lats,brock.T,cmap='Reds_r')
+    plt.ylabel('Latitude')
+    plt.title('Bedrock height [m]')
+    plt.ylim(-90,90)
+    plt.yticks([-60,-30,0,30,60])
     plt.xlabel('Time [years]')
     if xrange:
       plt.xlim(xrange)
-
-  if dir == '.':
-    dir = 'cwd'
+    plt.colorbar(c,cax=plt.axes([pos[1,0]+0.01,pos[0,1],0.01,pos[1,1]-pos[0,1]]))
   
-  if xrange:
-    sfile = 'evol_'+dir+'_%d_%d.pdf'%(xrange[0],xrange[1])
-  else:
-    sfile = 'evol_'+dir+'.pdf'
-  plt.savefig(sfile)
-  plt.close()
+
+    # plot insolation
+    insol = np.reshape(body.AnnInsol,(ntimes,nlats))
+    if orbit == True:
+      ax5 = plt.subplot(4,2,2)
+    else:
+      ax5 = plt.subplot(5,nfiles,ii+nfiles+1)
+    pos = ax5.figbox.get_points()
+    c = plt.contourf(body.Time,lats,insol.T,cmap='plasma')
+    plt.ylabel('Latitude')
+    plt.title(r'Annual average insolation [w/m$^2$]')
+    plt.ylim(-90,90)
+    plt.yticks([-60,-30,0,30,60])
+    if xrange:
+      plt.xlim(xrange)
+    plt.colorbar(c,cax=plt.axes([pos[1,0]+0.01,pos[0,1],0.01,pos[1,1]-pos[0,1]]))
+
+    if orbit == True:
+      #obliquity
+      plt.subplot(4,2,4)
+      plt.plot(body.Time,obl,linestyle = 'solid',marker='None',color='darkblue',linewidth =2)
+      plt.ylabel('Obliquity')
+      if xrange:
+        plt.xlim(xrange)
+
+      #eccentricity
+      plt.subplot(4,2,6)
+      plt.plot(body.Time,ecc,linestyle = 'solid',marker='None',color='darkorchid',linewidth =2)
+      plt.ylabel('Eccentricity')
+      if xrange:
+        plt.xlim(xrange)
+
+      #e sin(obl) sin varpi
+      plt.subplot(4,2,8)
+      plt.plot(body.Time,esinv,linestyle = 'solid',marker='None',color='salmon',linewidth=2)
+      plt.ylabel('COPP')
+      plt.xlabel('Time [years]')
+      if xrange:
+        plt.xlim(xrange)
+
+    if dir[ii] == '.':
+      dir[ii] = 'cwd'
+  
+    if xrange:
+      sfile = 'evol_'+dir[ii]+'_%d_%d.pdf'%(xrange[0],xrange[1])
+    else:
+      sfile = 'evol_'+dir[ii]+'.pdf'
+    plt.savefig(sfile)
+    plt.close()
 
 
 def tempminmax(plname,dir='.',xrange=False,orbit=False):
